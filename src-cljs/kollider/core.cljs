5b0521713d721eb954adc6ebd152700e4141893d
Bryan Garza <brygarza@gmail.com>
2014-11-16 22:45:27 -0800

    Implement posting

--- src-cljs/kollider/core.cljs
+++ src-cljs/kollider/core.cljs
@@ -3,10 +3,43 @@
 
 (enable-console-print!)
 
-(defn hello-world []
-  [:h1 "Hello world!"])
+(def app-state
+  (r/atom
+    {:posts
+     [{:user "bryan" :message "This is post 1"}
+      {:user "jonh" :message "This is the second post"}]}))
+
+(defn update-posts! [f & args]
+  (apply swap! app-state update-in [:posts] f args))
+
+(defn add-post! [text]
+  (update-posts! conj {:user "bryan" :message text}))
+
+(defn post-status []
+  (let [val (r/atom "")]
+    (fn []
+      [:div
+       [:input {:type "text"
+                :placeholder "Share something"
+                :value @val
+                :on-change #(reset! val (-> % .-target .-value))}]
+       [:button {:on-click #((add-post! @val)
+                             (reset! val ""))}
+        "Post"]])))
+
+(defn show-post [p]
+  [:li
+   (str (get p :user) " - " (get p :message))])
+
+(defn post-list []
+  [:div
+   [:h1 "Latest"]
+   [::ul
+    (for [p (:posts @app-state)]
+      [show-post p]) ]
+   [post-status]])
 
 (defn start []
   (r/render-component
-    [hello-world]
+    [post-list]
     (.getElementById js/document "root")))
