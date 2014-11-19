817e8be3047a415e2741d3c4b98d587e9b7e817d
Bryan Garza <brygarza@gmail.com>
2014-11-19 11:08:11 -0800

    add routing deps

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [reagent "0.4.3"]
-                 [cljs-ajax "0.3.3"]]
+                 [cljs-ajax "0.3.3"]
+                 [secretary "1.2.1"]
+                 [compojure "1.2.1"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds [{:source-paths ["src-cljs"]
                         :compiler {:output-to "app.js"
